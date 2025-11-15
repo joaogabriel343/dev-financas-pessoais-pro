65f6b57f6b20c304c67cedfa4d17d669ca464132
João Gabriel <28090420joaogabriel@gmail.com>
2025-11-14 23:40:39 -0300

    feat: Adiciona lógica de backend para categorias

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-// import { mockCategories } from "@/data/mockData";
 import { Plus, Trash2 } from "lucide-react";
 import { useEffect, useMemo, useState } from "react";
 import { useToast } from "@/hooks/use-toast";
